8dadbd3ff1a3f32f9fb4ce178b56e35ce9b1f730
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-11-19 21:56:11 -0700

    Add Postgres setup

--- ncviewjs-backend/app/main.py
+++ ncviewjs-backend/app/main.py
@@ -7,4 +7,9 @@ app = FastAPI()
 
 @app.get('/ping')
 async def ping(settings: Settings = Depends(get_settings)):
-    return {'ping': 'pong!', 'environment': settings.environment, 'testing': settings.testing}
+    return {
+        'ping': 'pong!',
+        'environment': settings.environment,
+        'testing': settings.testing,
+        'database_url': settings.database_url,
+    }
