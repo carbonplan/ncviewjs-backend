c0ccf095253569d685a22abf804ef9490c3bee2a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-11-20 18:22:00 -0700

    Database init

--- ncviewjs-backend/app/db.py
+++ ncviewjs-backend/app/db.py
@@ -1,4 +1,11 @@
+import logging
 import os
+
+from fastapi import FastAPI
+from tortoise import Tortoise, run_async
+from tortoise.contrib.fastapi import register_tortoise
+
+logger = logging.getLogger("uvicorn")
 
 TORTOISE_ORM = {
     "connections": {"default": os.environ.get("DATABASE_URL")},
@@ -9,3 +16,30 @@ TORTOISE_ORM = {
         },
     },
 }
+
+
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app,
+        db_url=os.environ["DATABASE_URL"],
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
+
+
+async def generate_schemas():
+
+    logger.info("Initializing Tortoise...")
+    await Tortoise.init(
+        db_url=os.environ["DATABASE_URL"],
+        modules={"models": ["models.tortoise"]},
+    )
+
+    logger.info("Generating database schema via Tortoise...")
+    await Tortoise.generate_schemas()
+    await Tortoise.close_connections()
+
+
+if __name__ == "__main__":
+    run_async(generate_schemas())
