2baac6f4b088c96c929928136075f3454641d10c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-11-21 19:48:58 -0700

    Update schema

--- ncviewjs-backend/app/models/tortoise.py
+++ ncviewjs-backend/app/models/tortoise.py
@@ -4,11 +4,11 @@ from tortoise import fields, models
 class Store(models.Model):
     id = fields.IntField(pk=True)
     url = fields.CharField(max_length=255, unique=True)
-    registered_at = fields.DatetimeField(auto_now_add=True)
-    updated_at = fields.DatetimeField(null=True)
-    status = fields.CharField(max_length=255, default='queued')
+    status = fields.CharField(max_length=255, null=True)
     conclusion = fields.CharField(max_length=255, null=True)
     rechunked_url = fields.CharField(max_length=255, null=True)
+    registered_at = fields.DatetimeField(auto_now_add=True)
+    last_accessed_at = fields.DatetimeField(auto_now=True)
 
     def __str__(self):
         return self.url
