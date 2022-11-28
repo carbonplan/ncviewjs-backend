161bb19d7bb4e7902ca1b3ad38e08bb4efcecd68
Raphael Hagen <norlandrhagen@gmail.com>
2022-11-28 16:53:43 -0700

    updated production, staging paths, added start of skyplane copying

--- ncviewjs-backend/app/rechunker/rechunk.py
+++ ncviewjs-backend/app/rechunker/rechunk.py
@@ -4,15 +4,16 @@ import xarray as xr
 import zarr
 from prefect import flow, task
 from utils import determine_chunk_size
-
+import os 
 from app.models.pydantic import SanitizedURL
 
 
 def _generate_tgt_tmp_stores(sanitized_url: SanitizedURL, processing_type: str) -> dict:
     store_suffix = f"/{processing_type}/{sanitized_url.bucket}/{sanitized_url.key}"
-    tmp_store = "s3://carbonplan-data-viewer-staging" + store_suffix
-    tgt_store = "s3://carbonplan-data-viewer-production" + store_suffix
-    return {'target_store': tgt_store, 'temp_store': tmp_store}
+    tmp_store = "s3://carbonplan-data-viewer-staging/tmp/" + store_suffix
+    staging_store = "s3://carbonplan-data-viewer-staging" + store_suffix
+    prod_store = "s3://carbonplan-data-viewer-production" + store_suffix
+    return {'temp_store': tmp_store, 'staging_store': staging_store, 'prod_store': prod_store}
 
 
 def _retrieve_CF_dims(url: str) -> dict:
@@ -23,6 +24,16 @@ def _retrieve_CF_dims(url: str) -> dict:
     Y = ds.cf['Y'].name
     T = ds.cf['T'].name
     return {'X': X, 'Y': Y, 'T': T}
+
+@task
+def copy_staging_to_production(store_paths: dict):
+    transfer_str = f"skyplane cp -r {store_paths['staging_store']} {store_paths['prod_store']}"
+    os.system(transfer_str)
+
+@task
+def finalize():
+    # TODO: finalize task to post info back to DB
+    pass
 
 
 @task
@@ -77,18 +88,19 @@ def rechunk_dataset(
         }
 
     tmp_mapper = fsspec.get_mapper(store_paths['temp_store'])
-    tgt_mapper = fsspec.get_mapper(store_paths['target_store'])
+    tgt_mapper = fsspec.get_mapper(store_paths['staging_store'])
 
     array_plan = rechunker.rechunk(group, chunks_dict, max_mem, tgt_mapper, temp_store=tmp_mapper)
     array_plan.execute()
-    return store_paths['target_store']
 
 
 @flow(name="rechunking-flow")
 def rechunk_flow(sanitized_url: SanitizedURL) -> dict:
     store_paths = _generate_tgt_tmp_stores(sanitized_url, processing_type='rechunked')
     cf_dims_dict = _retrieve_CF_dims(sanitized_url.url)
-    target_store = rechunk_dataset(
+    rechunk_dataset(
         zarr_store_url=sanitized_url.url, cf_dims_dict=cf_dims_dict, store_paths=store_paths
     )
-    return {'target_store': target_store}
+    copy_staging_to_production(store_paths)
+    finalize()
+    return {'production_store': store_paths['prod_store']}
