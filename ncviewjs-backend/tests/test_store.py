cb4f33a057f5eae3fa565b00f895252c02492462
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-11-28 09:54:14 -0700

    Update tests

--- ncviewjs-backend/tests/test_store.py
+++ ncviewjs-backend/tests/test_store.py
@@ -75,3 +75,31 @@ def test_get_store_not_found(test_app_with_db):
     assert response.status_code == 404
     data = response.json()
     assert f"Store: {url} not found" in data["detail"]
+
+
+def test_post_store_md5_id(test_app_with_db):
+
+    a = "s3://carbonplan-share/cmip6-downscaling/DeepSD/"
+    b = "https://carbonplan-share.s3.us-west-2.amazonaws.com/cmip6-downscaling/DeepSD/"
+
+    response = test_app_with_db.post(
+        '/store/',
+        content=json.dumps({"url": a}),
+    )
+
+    assert response.status_code == 201
+
+    response_2 = test_app_with_db.post(
+        '/store/',
+        content=json.dumps({"url": b}),
+    )
+
+    assert response_2.status_code == 201
+
+    data = response.json()
+    data_2 = response_2.json()
+
+    assert data['md5_id'] == data_2['md5_id']
+    assert data['bucket'] == data_2['bucket']
+    assert data['key'] == data_2['key']
+    assert data['protocol'] != data_2['protocol']
