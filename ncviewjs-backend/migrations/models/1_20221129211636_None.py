558ee9062f615392563b8e57225b1f793cbb9400
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2022-11-29 15:10:23 -0700

    Remove unnecessary migrations (#15)

--- ncviewjs-backend/migrations/models/1_20221129211636_None.py
+++ ncviewjs-backend/migrations/models/1_20221129211636_None.py
@@ -6,11 +6,16 @@ async def upgrade(db: BaseDBAsyncClient) -> str:
         CREATE TABLE IF NOT EXISTS "store" (
     "id" SERIAL NOT NULL PRIMARY KEY,
     "url" VARCHAR(255) NOT NULL UNIQUE,
+    "md5_id" VARCHAR(255) NOT NULL,
+    "protocol" VARCHAR(255) NOT NULL,
+    "key" VARCHAR(255) NOT NULL,
+    "bucket" VARCHAR(255) NOT NULL,
     "status" VARCHAR(255),
     "conclusion" VARCHAR(255),
     "rechunked_url" VARCHAR(255),
     "registered_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-    "last_accessed_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
+    "last_accessed_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+    "error_message" TEXT
 );
 CREATE TABLE IF NOT EXISTS "aerich" (
     "id" SERIAL NOT NULL PRIMARY KEY,
