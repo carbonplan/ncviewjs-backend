f9e14a10e894781b504551c963f1cf37a6215ce4
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-12-08 17:43:33 -0700

    Update /runs/ endpoints

--- ncviewjs_backend/api/runs.py
+++ ncviewjs_backend/api/runs.py
@@ -1,4 +1,6 @@
+import fastapi.responses
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 from sqlmodel import Session, select
 
 from ..database import get_session
@@ -12,3 +14,18 @@ logger = get_logger()
 @router.get("/", response_model=list[RechunkRun], summary="Get a list of all rechunk runs")
 def list_rechunk_runs(session: Session = Depends(get_session)):
     return session.exec(select(RechunkRun)).all()
+
+
+@router.get("/{id}", response_model=RechunkRun, summary="Get a rechunk run by id")
+def get_rechunk_run(id: int, session: Session = Depends(get_session)):
+    try:
+        return session.exec(select(RechunkRun).where(RechunkRun.id == id)).one()
+    except NoResultFound:
+        return fastapi.responses.JSONResponse(
+            status_code=404, content={"detail": f"Run with {id} not found"}
+        )
+
+    except MultipleResultsFound:
+        return fastapi.responses.JSONResponse(
+            status_code=500, content={"detail": f"Multiple runs found for {id}"}
+        )
