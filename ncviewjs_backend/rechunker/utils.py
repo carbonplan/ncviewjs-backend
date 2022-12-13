e22f87f6aab6f8a19fc76a1e7bb5aa40ac28f0d2
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-12-13 12:41:22 -0700

    use spatial_chunk_square_size

--- ncviewjs_backend/rechunker/utils.py
+++ ncviewjs_backend/rechunker/utils.py
@@ -1,4 +1,2 @@
-def determine_chunk_size(
-    spatial_chunk_square_size: int = 128, target_size_bytes: int = 5e5
-) -> int:  # ex 500 KB
-    return round((target_size_bytes / 4) / (128 * 128))
+def determine_chunk_size(spatial_chunk_square_size: int = 256, target_size_bytes: int = 5e5) -> int:
+    return round(target_size_bytes / 4 / spatial_chunk_square_size**2)
