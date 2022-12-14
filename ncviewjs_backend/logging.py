67cbea0cb8a1dde36dacfac095a0642a7f0242d9
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-12-14 16:42:50 -0700

    use proper logger

--- ncviewjs_backend/logging.py
+++ ncviewjs_backend/logging.py
@@ -1,5 +1,10 @@
 import logging
 
+from .config import get_settings
+
 
 def get_logger() -> logging.Logger:
+    settings = get_settings()
+    if settings.environment == 'prod':
+        return logging.getLogger("gunicorn")
     return logging.getLogger("uvicorn")
