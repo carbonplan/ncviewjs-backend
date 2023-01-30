d77596c811607f48516acd0d2f31cea783619aa7
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-01-30 16:37:09 -0700

    remove unnecessary test

--- tests/test_dataset_processing.py
+++ tests/test_dataset_processing.py
@@ -1,7 +1,6 @@
 import pytest
 
 from ncviewjs_backend.dataset_processing import (
-    DatasetTooLargeError,
     UnableToOpenDatasetError,
     validate_zarr_store,
 )
@@ -11,11 +10,3 @@ def test_unable_to_open_store_error():
     """Test that the Zarr store is accessible"""
     with pytest.raises(UnableToOpenDatasetError):
         validate_zarr_store('https://raw.githubusercontent.com/foo/bar/baz.zarr')
-
-
-def test_too_large_store_error():
-    """Test that the dataset is not too large to be processed"""
-    with pytest.raises(DatasetTooLargeError):
-        validate_zarr_store(
-            'gs://cmip6/CMIP6/CMIP/NOAA-GFDL/GFDL-CM4/historical/r1i1p1f1/Omon/thetao/gn/v20180701/'
-        )
